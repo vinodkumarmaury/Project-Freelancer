29faa8ea1a5fa0dad64ae462920a9ddbbae6cc44
Vinod Kumar Maurya <vinodmaurya0410@gmail.com>
2025-04-06 02:12:22 +0530

    feat: Integrate next-transpile-modules for Radix UI components in configuration

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const withTM = require('next-transpile-modules')([
+  '@radix-ui/react-dialog',
+  '@radix-ui/react-tooltip',
+  // add any other radix packages you use
+]);
+
+module.exports = withTM({
   reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
-};
-
-module.exports = nextConfig;
+});
